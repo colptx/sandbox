d9d024efc382470eb6addd9c71cbc249c63d6dd3
TASEALS <58431495+TASEALS@users.noreply.github.com>
2021-12-27 20:57:30 -0600

    Update compair.py
    modified for print for 3.6+ as no one should be using 2.7 due to security risks.

--- Python/compair.py
+++ Python/compair.py
@@ -1,35 +1,35 @@
-#
-#
-#
+def main():
+    #Check if firstNum value entered is a number
+    while True:
+        try:
+            firstNum = int(input("Enter first number:"))
+        except ValueError:
+            print("Please enter a number.")
+            continue
+        else:
+            #firstNum was successfully parsed!
+            #exit loop
+            break
 
-#Check if firstnum value entered is a number
-while True:
-    try:
-        firstnum = int(input("Enter first number:"))
-    except ValueError:
-        print("Please enter a number.")
-        continue
+    #Check if secondNum value entered is a number
+    while True:
+        try:
+            secondNum = int(input("Enter second number:"))
+        except ValueError:
+            print("Please enter a number.")
+            continue
+        else:
+            #firstNum was successfully parsed!
+            #exit loop
+            break
+    
+    # Compairison Evaluation 
+    if firstNum > secondNum:
+        print(f"{firstNum} is greater than {secondNum}!")
+    elif firstNum < secondNum:
+        print(f"{firstNum} is less than {secondNum}!")
     else:
-        #firstnum was successfully parsed!
-        #exit loop
-        break
+        print(f"{firstNum} and {secondNum} are equal.")
 
-#Check if secondnum value entered is a number
-while True:
-    try:
-        secondnum = int(input("Enter second number:"))
-    except ValueError:
-        print("Please enter a number.")
-        continue
-    else:
-        #firstnum was successfully parsed!
-        #exit loop
-        break
- 
-# Compairison Evaluation 
-if firstnum > secondnum:
-    print(str(firstnum) + " is greater than " + str(secondnum) + " !")
-elif firstnum < secondnum:
-    print(str(firstnum) + " is less than " + str(secondnum) + " !")
-else:
-    print( str(firstnum) + " and " + str(secondnum) + " are equal." )
+if __name__ == '__main__':
+    main()
